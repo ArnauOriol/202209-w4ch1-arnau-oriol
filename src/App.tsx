6788eef6eff561116efb00e477723ba33388697f
Arnau Oriol <arnauoriol1996@hotmail.com>
2022-10-17 21:28:47 +0200

    Add Info component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import Info from "./Info/Info";
+
 const App = () => {
   return (
-    <div className="container">
-      <header className="main-header">
-        <h1 className="main-title">The pointing gentlemen</h1>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <header className="main-header">
+          <h1 className="main-title">The pointing gentlemen</h1>
+        </header>
+      </div>
+      <section className="controls">
+        <Info></Info>
+      </section>
+    </>
   );
 };
 
